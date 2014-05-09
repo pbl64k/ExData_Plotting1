ff0c290c92afe6ce4c7486bc7a0c24b3769b06a7
pbl64k <pbl64k@gmail.com>
2014-05-09 23:30:52 +0300

    Plot 4.

--- plot4.R
+++ plot4.R
@@ -4,15 +4,31 @@ epc <- read.table('./household_power_consumption.txt', sep = ';', header = T)
 
 epc <- epc[epc$Date == '1/2/2007' | epc$Date == '2/2/2007',]
 
+epc$Global_active_power <- as.numeric(as.character(epc$Global_active_power))
+
 epc$Sub_metering_1 <- as.numeric(as.character(epc$Sub_metering_1))
 epc$Sub_metering_2 <- as.numeric(as.character(epc$Sub_metering_2))
 epc$Sub_metering_3 <- as.numeric(as.character(epc$Sub_metering_3))
 
-epc$datetime <- strptime(paste(epc$Date, epc$Time), format = '%d/%m/%Y %H:%M:%S')
+epc$Voltage <- as.numeric(as.character(epc$Voltage))
+
+epc$Global_reactive_power <-
+    as.numeric(as.character(epc$Global_reactive_power))
+
+epc$datetime <- strptime(paste(epc$Date, epc$Time),
+                         format = '%d/%m/%Y %H:%M:%S')
 
 # Apparently we need to plot directly to PNG device for the legend to render
 # properly.
-png(filename = './plot3.png', width = 480, height = 480)
+png(filename = './plot4.png', width = 480, height = 480)
+
+par(mfcol=c(2, 2))
+
+plot(epc$datetime, epc$Global_active_power, pch = '',
+     xlab = '',
+     ylab = 'Global Active Power',
+     main = '')
+lines(epc$datetime, epc$Global_active_power)
 
 plot(epc$datetime, epc$Sub_metering_1, pch = '',
      xlab = '',
@@ -24,6 +40,19 @@ lines(epc$datetime, epc$Sub_metering_3, col = 'blue')
 legend('topright', 'top',
        legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
        col = c('black', 'red', 'blue'),
+       border = 'white',
        lwd = 1)
+
+plot(epc$datetime, epc$Voltage, pch = '',
+     xlab = 'datetime',
+     ylab = 'Voltage',
+     main = '')
+lines(epc$datetime, epc$Voltage)
+
+plot(epc$datetime, epc$Global_reactive_power, pch = '',
+     xlab = 'datetime',
+     ylab = 'Global_reactive_power',
+     main = '')
+lines(epc$datetime, epc$Global_reactive_power)
 
 dev.off()
